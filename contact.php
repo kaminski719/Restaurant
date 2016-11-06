226c96bea937204ea72bb4dd67ff3260d501a26b
kaminski719 <kaminski719@gmail.com>
2016-11-06 16:37:59 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,4 @@
+<?php include('header.php'); ?>
+<h1>CONTACT US</h1>
+<p>&nbsp;</p>
+<?php include('footer.php'); ?>
---
+++ header.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<link rel="stylesheet" href="homestyle.css" type="text/css" />
+<title>Restaurant</title>
+</head>
+<body>
+<div id="header">
+<center><p><h1>CAPSTONE RESTAURANT</h1></p></center>
+</div>
+
+<center>
+  <p>
+  <div class="navlink">
+  	<a href="index.php">HOME</a>
+  </div>
+  <div class="navlink">
+  	<a href="menu.php">MENU</a>
+  </div>
+  
+  <div class="navlink">
+  	<a href="news.php">NEWS</a>
+  </div>
+  <div class="navlink">
+ 	 <a href="events.php">EVENTS</a>
+  </div>
+  <div class="navlink">
+ 	 <a href="about.php">ABOUT US</a>
+  </div>
+  <div class="navlink">
+  	<a href="contact.php">CONTACT US</a>
+  </div>
+  </p>
+ </center>
+
+
