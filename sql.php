c0f300fc0dc54874378428bba1823ba5cef71865
kaminski719 <kaminski719@gmail.com>
2016-12-16 03:55:51 -0500

    Delete sql.php

--- sql.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$host = 'db.soic.indiana.edu';
-	$user = 'i491u16_mickamin';
-	$db = 'i491u16_mickamin';
-	$pass = 'my+sql=i491u16_mickamin';
-	
-$connect = @mysql_connect($host, $user, $pass) or die('Could not connect to database...');
-$select = @mysql_select_db($db, $connect) or die('That database could not be found.');
-?>
